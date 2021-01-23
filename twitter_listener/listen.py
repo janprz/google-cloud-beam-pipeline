47bc48bb3775655d6ceed8ad4f5886d2ebf5c2ce
janiepe <jasiek.przybyszewski@gmail.com>
2021-01-23 13:55:31 +0100

    Add twitter text publishing to sns from twitter stream.

--- twitter_listener/listen.py
+++ twitter_listener/listen.py
@@ -1,8 +1,10 @@
 import listener
 import tweepy
 
+
 if __name__ == '__main__':
     stream_listener = listener.TwitterStreamListener()
     stream = tweepy.Stream(auth=stream_listener.api.auth, listener=stream_listener)
     hashtags = ['#Cyberpunk2077']
     stream.filter(track=hashtags)
+    # stream_listener.test_sns_publishing()
--- twitter_listener/listener.py
+++ twitter_listener/listener.py
@@ -1,17 +1,57 @@
 import tweepy
 import os
+import json
+import boto3
 from dotenv import load_dotenv
 
 
 class TwitterStreamListener(tweepy.StreamListener):
     def __init__(self):
-        self.connect()
+        load_dotenv()
+        self.api = None
+        self.topic_arn = None
+        self.sns_client = None
+        self.initialize_twitter_api()
+        self.initialize_sns_client()
 
     def on_status(self, status):
         print(status.text)
+        publish_object = {'text': status.text}
+        response = self.sns_client.publish(
+            TopicArn=self.topic_arn,
+            Message=json.dumps(publish_object),
+            Subject='twitter_text')
+        print(response['ResponseMetadata']['HTTPStatusCode'])
 
-    def connect(self):
-        load_dotenv()
+
+    def initialize_twitter_api(self):
         auth = tweepy.OAuthHandler(os.getenv('CONSUMER_KEY'), os.getenv('CONSUMER_SECRET'))
         auth.set_access_token(os.getenv('ACCESS_TOKEN'), os.getenv('ACCESS_SECRET'))
         self.api = tweepy.API(auth)
+
+    def initialize_sns_client(self):
+        self.topic_arn = os.getenv('TOPIC_ARN')
+        self.sns_client = boto3.client(
+            'sns',
+            aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
+            aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
+            aws_session_token=os.getenv('AWS_SESSION_TOKEN'),
+            region_name=os.getenv('REGION_NAME')
+        )
+
+    # def test_sns_publishing(self):
+    #     topicArn = 'arn:aws:sns:us-east-1:528869935487:twitterTopic'
+    #     snsClient = boto3.client(
+    #         'sns',
+    #         aws_access_key_id = os.getenv('AWS_ACCESS_KEY_ID'),
+    #         aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
+    #         aws_session_token=os.getenv('AWS_SESSION_TOKEN'),
+    #         region_name=os.getenv('REGION_NAME')
+    #     )
+    #     publish_object = {'text': 'test_text_sent_from_pycharm_2'}
+    #     response = snsClient.publish(
+    #         TopicArn=topicArn,
+    #         Message=json.dumps(publish_object),
+    #         Subject='Test_message_pycharm')
+    #     print(response['ResponseMetadata']['HTTPStatusCode'])
+
