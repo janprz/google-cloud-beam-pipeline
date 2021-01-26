278a8d14a5eed3f7f108ca66c7ffcb9db2dbb8ca
Tomasz Goralczyk <t.goralczyk@ocado.com>
2021-01-26 20:02:35 +0100

    Dashborad with sentiment analysis, word/char count and wordcloud.

--- processing_pipeline/beam_pipeline.py
+++ processing_pipeline/beam_pipeline.py
@@ -7,6 +7,7 @@ import logging
 import os
 from dotenv import load_dotenv
 import numpy as np
+from collections import Counter
 
 import apache_beam as beam
 import apache_beam.transforms.window as window
@@ -14,36 +15,65 @@ from apache_beam.io.gcp.bigquery_tools import parse_table_schema_from_json
 from apache_beam.options.pipeline_options import StandardOptions, GoogleCloudOptions, SetupOptions, PipelineOptions
 from apache_beam.transforms.util import BatchElements
 
+from google.cloud import language
+
 
 def predict_sentiment(messages):
     if not isinstance(messages, list):
         messages = [messages]
 
+    lang_client = language.LanguageServiceClient()
     instances = list(map(lambda message: json.loads(message), messages))
 
-    scores = [1 for instance in instances]
-
     for i, instance in enumerate(instances):
-        instance['sentiment'] = scores[i]
+        document = language.types.Document(content=instance['text'], type=language.types.Document.Type.PLAIN_TEXT)
+        annotated_text = lang_client.annotate_text(document=document, features={'extract_syntax':True, 'extract_document_sentiment':True})
+        #print('>>> sentiment', sentiment)
+        instance['sentiment_score'] = annotated_text.document_sentiment.score
+        instance['sentiment_magnitude'] = annotated_text.document_sentiment.magnitude
+        instance['language'] = annotated_text.language
 
     logging.info("first message")
     logging.info(instances[0])
 
     return instances
 
+def analyze_batch(messages):
+    if not isinstance(messages, list):
+        messages = [messages]
+
+    lang_client = language.LanguageServiceClient()
+    instances = list(map(lambda message: json.loads(message), messages))
+    result = {}
+    total_words = 0
+    total_characters = 0
+    total_sentiment_score = 0
+    top_words = Counter()
+    top_langs = Counter()
+    for i, instance in enumerate(instances):
+        document = language.types.Document(content=instance['text'], type=language.types.Document.Type.PLAIN_TEXT)
+        annotated_text = lang_client.annotate_text(document=document, features={'extract_syntax':True, 'extract_document_sentiment':True})
+        top_words.update(Counter([t.lemma for t in annotated_text.tokens]))
+        top_langs[annotated_text.language] += 1
+        total_words += len(instance['text'].split(' '))
+        total_characters += len(instance['text'])
+        total_sentiment_score += annotated_text.document_sentiment.score
+        break
+
+    result['time_stamp'] = instances[-1]['time_stamp']
+    result['avg_num_words'] = total_words / len(instances)
+    result['avg_num_characters'] = total_characters / len(instances)
+    result['avg_sentiment_score'] = total_sentiment_score / len(instances)
+    result['top_words'] = ",".join([t[0] for t in top_words.most_common(10)])
+    result['top_languages'] = ",".join([t[0] for t in top_langs.most_common(10)])
+    result['batch_size'] = len(messages)
+    logging.info(">>> batch result")
+    logging.info(result)
+    return result
+
 
 def run(argv=None):
     load_dotenv()
-
-    bigqueryschema_json = json.dumps({
-        "fields": [{"name": "id", "type": "STRING"},
-                   {"name": "time_stamp", "type": "TIMESTAMP"},
-                   {"name": "text", "type": "STRING"},
-                   {"name": "username", "type": "STRING"},
-                   {"name": "sentiment", "type": "INTEGER"},
-                   {"name": "n_followers", "type": "INTEGER"}]
-    })
-    bigqueryschema = parse_table_schema_from_json(bigqueryschema_json)
 
     parser = argparse.ArgumentParser()
     group = parser.add_mutually_exclusive_group(required=False)
@@ -67,7 +97,11 @@ def run(argv=None):
     pipeline_options.view_as(SetupOptions).save_main_session = True
     pipeline_options.view_as(StandardOptions).streaming = True
     pipeline_options.view_as(StandardOptions).runner = 'DirectRunner'
+    
+    result = run_pipeline(pipeline_options, known_args)
+    result.wait_until_finish()
 
+def run_pipeline(pipeline_options, known_args):
     p = beam.Pipeline(options=pipeline_options)
     lines = p | "read in tweets" >> beam.io.ReadFromPubSub(
         subscription=known_args.input_subscription,
@@ -75,20 +109,55 @@ def run(argv=None):
         # id_label="tweet_id" # not for direct runner
     )
     output_tweets = (lines
-                     | 'add window key' >> beam.WindowInto(window.FixedWindows(10))
+                     | 'add window key' >> beam.WindowInto(window.FixedWindows(10)) # 10 seconds
                      | 'batch messages' >> BatchElements(min_batch_size=2, max_batch_size=50)
                      | 'predict sentiment' >> beam.FlatMap(lambda messages: predict_sentiment(messages))
                      )
+    bq_schema_tweets = json.dumps({
+        "fields": [{"name": "id", "type": "STRING"},
+                   {"name": "time_stamp", "type": "TIMESTAMP"},
+                   {"name": "text", "type": "STRING"},
+                   {"name": "username", "type": "STRING"},
+                   {"name": "sentiment", "type": "INTEGER"},
+                   {"name": "sentiment_score", "type": "FLOAT"},
+                   {"name": "sentiment_magnitude", "type": "FLOAT"},
+                   {"name": "language", "type": "STRING"},
+                   {"name": "n_followers", "type": "INTEGER"}]
+    })
     output_tweets | 'write to BQ' >> beam.io.WriteToBigQuery(
         table=os.getenv('BQ_TABLE'),
         dataset=os.getenv('BQ_DATASET'),
-        schema=bigqueryschema,
+        schema=parse_table_schema_from_json(bq_schema_tweets),
         write_disposition=beam.io.BigQueryDisposition.WRITE_APPEND,
         create_disposition=beam.io.BigQueryDisposition.CREATE_IF_NEEDED,
         project=os.getenv('GC_PROJECT')
     )
-    result = p.run()
-    result.wait_until_finish()
+    
+    output_batch = (lines
+                     | 'add window key 2' >> beam.WindowInto(window.FixedWindows(1 * 60)) # 1 minute
+                     | 'batch messages 2' >> BatchElements(min_batch_size=10)
+                     | 'analyze in batch' >> beam.Map(lambda messages: analyze_batch(messages))
+                     )
+
+    bq_schema_batches = json.dumps({
+        "fields": [{"name": "time_stamp", "type": "TIMESTAMP"},
+                   {"name": "batch_size", "type": "INTEGER"},
+                   {"name": "top_words", "type": "STRING"},
+                   {"name": "top_languages", "type": "STRING"},
+                   {"name": "avg_num_words", "type": "FLOAT"},
+                   {"name": "avg_num_characters", "type": "FLOAT"},
+                   {"name": "avg_sentiment_score", "type": "FLOAT"}]
+    })
+    output_batch | 'write to BQ 2' >> beam.io.WriteToBigQuery(
+        table=os.getenv('BQ_TABLE_BATCH'),
+        dataset=os.getenv('BQ_DATASET'),
+        schema=parse_table_schema_from_json(bq_schema_batches),
+        write_disposition=beam.io.BigQueryDisposition.WRITE_APPEND,
+        create_disposition=beam.io.BigQueryDisposition.CREATE_IF_NEEDED,
+        project=os.getenv('GC_PROJECT')
+    )
+
+    return p.run()
 
 
 if __name__ == '__main__':
--- visualization/big_query_visualization.py
+++ visualization/big_query_visualization.py
@@ -10,7 +10,12 @@ import dash_html_components as html
 import pandas as pd
 import plotly.graph_objs as go
 import plotly
+
 from dash.dependencies import Output, Input
+from io import BytesIO
+from wordcloud import WordCloud
+import base64
+from collections import Counter
 
 load_dotenv()
 
@@ -23,38 +28,87 @@ app.title = 'Dash application'
 
 app.layout = html.Div(
     [
-        dcc.Graph(id='live-graph', animate=True),
+        dcc.Graph(id='live-graph', animate=True, style={'height': '90vh'}),
+        dcc.Graph(id='batch-graph', animate=True, style={'height': '90vh'}),
         dcc.Interval(
             id='interval-component',
             interval=5*1000,
             n_intervals=0
         ),
+        html.Img(id="image_wc")
     ]
 )
 
 @app.callback(Output('live-graph', 'figure'),
               Input('interval-component', 'n_intervals'))
 def update_plot(n):
-    sql = "SELECT * FROM `adzd-project-302811.adzd_twitter_dataset.adzd_twitter_table` LIMIT 1000"
+    sql = "SELECT * FROM `adzd-project-302811.adzd_twitter_dataset.adzd_twitter_table` ORDER BY time_stamp DESC LIMIT 10000"
 
     df = pandas.read_gbq(sql, dialect='standard', use_bqstorage_api=False,project_id=os.getenv('GC_PROJECT'))
 
     df = df.set_index('time_stamp')
     df = df.sort_index()
-    fig = plotly.subplots.make_subplots(rows=1, cols=1)
+    fig = plotly.subplots.make_subplots(rows=2, cols=1)
     fig.append_trace({
         'x': df.index,
         'y': df.n_followers,
-        'name': 'Tweets',
-        'mode': 'lines+markers',
-        'type': 'scatter'
+        'name': 'Followers',
+        'mode': 'markers',
+        'type': 'scatter',
+        'text': df.text
     }, 1, 1)
+    fig.append_trace({
+        'x': df.index,
+        'y': df.sentiment_score,
+        'name': 'Sentiments',
+        'mode': 'markers',
+        'type': 'scatter',
+        'text': df.text
+    }, 2, 1)
     return fig
+
+@app.callback(Output('batch-graph', 'figure'),
+              Input('interval-component', 'n_intervals'))
+def update_plot(n):
+    sql = "SELECT * FROM `adzd-project-302811.adzd_twitter_dataset.adzd_twitter_table_batch` ORDER BY time_stamp DESC LIMIT 10000"
+
+    df = pandas.read_gbq(sql, dialect='standard', use_bqstorage_api=False,project_id=os.getenv('GC_PROJECT'))
+
+    df = df.set_index('time_stamp')
+    df = df.sort_index()
+    fig = plotly.subplots.make_subplots(rows=2, cols=1)
+    fig.append_trace({
+        'x': df.index,
+        'y': df.avg_num_words,
+        'name': 'Avg number of words'
+    }, 1, 1)
+    fig.append_trace({
+        'x': df.index,
+        'y': df.avg_num_characters,
+        'name': 'Avg number of characters'
+    }, 2, 1)
+    return fig
+
+def plot_wordcloud(data):
+    wc = WordCloud(background_color='black', width=800, height=600)
+    wc.fit_words(data)
+    return wc.to_image()
+
+@app.callback(Output('image_wc', 'src'), Input('image_wc', 'id'))
+def update_wc(n):
+    sql = "SELECT * FROM `adzd-project-302811.adzd_twitter_dataset.adzd_twitter_table_batch` ORDER BY time_stamp DESC LIMIT 10"
+
+    df = pandas.read_gbq(sql, dialect='standard', use_bqstorage_api=False,project_id=os.getenv('GC_PROJECT'))
+
+    df = df.set_index('time_stamp')
+    df = df.sort_index()
+
+    cnt = Counter()
+    [cnt.update(Counter(top)) for top in df.top_words.str.split(',')]
+    img = BytesIO()
+    plot_wordcloud(data={x:y for x,y in cnt.most_common(40)}).save(img, format='PNG')
+    return 'data:image/png;base64,{}'.format(base64.b64encode(img.getvalue()).decode())
 
 
 if __name__ == '__main__':
     app.run_server(debug=True)
-
-
-
-
